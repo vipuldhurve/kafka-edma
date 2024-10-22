a8cada601de74c0112222a446098acfb1ae4b789
vipuldhurve <vipuldhurve@gmail.com>
2024-10-22 23:51:06 +0530

    Added project for event driven microservices architecture using apache kafka messaging

---
+++ base-domains/src/main/java/com/kafka/base_domains/dto/Order.java
@@ -0,0 +1,15 @@
+package com.kafka.base_domains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String id;
+    private String name;
+    private int quantity;
+    private double price;
+}
---
+++ base-domains/src/test/java/com/kafka/base_domains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.base_domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
